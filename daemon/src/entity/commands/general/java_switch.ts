83764dd913955ff26f9a4a8a0a4234acaee8962b
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-03-03 19:38:06 +0800

    Update java_switch.ts

--- daemon/src/entity/commands/general/java_switch.ts
+++ daemon/src/entity/commands/general/java_switch.ts
@@ -1,8 +1,5 @@
-// 根據你提供的層級，回跳三層到 src 根目錄
 import { $t } from "../../../i18n";
-// 回跳兩層到 entity 找到實例類
 import Instance from "../../instance/instance";
-// 回跳兩層到 commands 找到基類
 import InstanceCommand from "../base/command";
 
 export default class JavaSwitchCommand extends InstanceCommand {
@@ -11,9 +8,8 @@ export default class JavaSwitchCommand extends InstanceCommand {
   }
 
   async exec(instance: Instance, params?: any) {
-    // 檢查狀態
     if (instance.status() !== Instance.STATUS_STOP) {
-      return instance.failure(new Error($t("請先停止伺服器後再切換 Java 版本")));
+      return instance.failure(new Error($t("請先停止伺服器後再更換 Java 版本")));
     }
 
     const newCommand = params?.startCommand;
@@ -22,26 +18,29 @@ export default class JavaSwitchCommand extends InstanceCommand {
     try {
       instance.setLock(true);
       instance.status(Instance.STATUS_BUSY);
-      instance.println("System", `請稍候`);
+      instance.println("System", `正在切換啟動指令為: ${newCommand}`);
 
-      // 修改配置
+      // 1. 修改內存中的配置
       instance.config.startCommand = newCommand;
       
-      // 保存更改
-      await instance.save();
+      // 2. 嘗試調用配置對象的保存方法
+      // 在多數 MCSManager 版本中，配置持久化是通過 instance.config.save()
+      if (typeof (instance.config as any).save === 'function') {
+          await (instance.config as any).save();
+      } else {
+          // 如果沒有 save，嘗試保存更新
+          await (instance.config as any).saveUpdate();
+      }
 
-      instance.println("System", "Java 切換成功！");
+      instance.println("System", "Java 環境切換成功！");
     } catch (err: any) {
       instance.println("System", `切換失敗: ${err.message}`);
     } finally {
-      // 恢復狀態
       instance.asynchronousTask = undefined;
       instance.setLock(false);
       instance.status(Instance.STATUS_STOP);
     }
   }
 
-  async stop(instance: Instance): Promise<void> {
-    // 此類任務通常瞬間完成，不需額外停止邏輯
-  }
+  async stop(instance: Instance): Promise<void> {}
 }
