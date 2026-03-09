105fd43e555b95260413846dd469fdea4b16e43d
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-03-09 19:37:04 +0800

    Update general_install.ts

--- daemon/src/entity/commands/general/general_install.ts
+++ daemon/src/entity/commands/general/general_install.ts
@@ -1,4 +1,5 @@
 import fs from "fs-extra";
+import path from "path"; // 新增：用於處理路徑拼接
 import { $t } from "../../../i18n";
 import { QuickInstallTask } from "../../../service/async_task_service/quick_install";
 import Instance from "../../instance/instance";
@@ -23,23 +24,51 @@ export default class GeneralInstallCommand extends InstanceCommand {
     if (instance.asynchronousTask)
       return instance.failure(new Error($t("TXT_CODE_general_update.statusErr_otherProgress")));
     if (!params) throw new Error("GeneralInstallCommand: No params");
+    
     try {
       instance.setLock(true);
       instance.status(Instance.STATUS_BUSY);
+      
+      // 輸出提示訊息
       instance.println($t("TXT_CODE_1704ea49"), $t("TXT_CODE_cbc235ad"));
+
       if (instance.hasCwdPath()) {
-        await fs.remove(instance.absoluteCwdPath());
-        await fs.mkdirs(instance.absoluteCwdPath());
+        const cwd = instance.absoluteCwdPath();
+        const backupDirName = "LazyCloud_backup"; // 定義要保護的目錄名
+
+        // 檢查路徑是否存在
+        if (await fs.pathExists(cwd)) {
+          // 讀取當前目錄下的所有檔案和資料夾
+          const items = await fs.readdir(cwd);
+          
+          for (const item of items) {
+            // 如果是備份目錄，則跳過
+            if (item === backupDirName) {
+              instance.println("INFO", `[LazyCloud] 保護備份目錄: ${item}，不執行刪除。`);
+              continue;
+            }
+            
+            // 執行遞迴刪除其他檔案
+            await fs.remove(path.join(cwd, item));
+          }
+        } else {
+          // 如果目錄根本不存在，則創建它
+          await fs.mkdirs(cwd);
+        }
       }
+
       instance.println($t("TXT_CODE_1704ea49"), $t("TXT_CODE_906c5d6a"));
+      
       this.installTask = new QuickInstallTask(
         instance.config.nickname,
         params.targetLink,
         params.setupInfo,
         instance
       );
+      
       instance.asynchronousTask = this;
       instance.println($t("TXT_CODE_1704ea49"), $t("TXT_CODE_b9ca022b"));
+      
       await this.installTask?.start();
       await this.installTask?.wait();
     } catch (err: any) {
