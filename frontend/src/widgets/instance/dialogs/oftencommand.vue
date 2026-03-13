2f9959bb0bf61e4e5a502e4a09dccc4f9248e05d
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-03-13 20:44:45 +0800

    Update oftencommand.vue

--- frontend/src/widgets/instance/dialogs/oftencommand.vue
+++ frontend/src/widgets/instance/dialogs/oftencommand.vue
@@ -195,7 +195,7 @@ defineExpose({ openDialog });
   >
     <div v-if="COMMAND_GROUPS.length > 0" class="quick-cmd-container">
       <div class="header-toolbar">
-        <span class="desc-text">{{ t("數據刷新可能有 1-5 分鐘延遲") }}</span>
+        <span class="desc-text">{{ t("玩家數據可能有 1-5 分鐘延遲，取決於 API 緩存") }}</span>
         <a-button class="refresh-btn" size="small" @click="fetchPlayers" :loading="isFetchingPlayers">
           <template #icon><ReloadOutlined /></template>
         </a-button>
