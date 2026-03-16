66c3e512692273fb904b8c5285dfb628a2eb19d7
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-03-16 22:18:53 +0800

    Update worldchange.vue

--- frontend/src/widgets/instance/dialogs/worldchange.vue
+++ frontend/src/widgets/instance/dialogs/worldchange.vue
@@ -51,7 +51,7 @@ const deepScanWorlds = async (targetPath: string, results: DetectedWorld[] = [],
 
   try {
     const res = await fetchFiles({
-      params: { daemonId: props.daemonId, uuid: props.instanceId, target: currentPath, page: 0, page_size: 150, file_name: "" }
+      params: { daemonId: props.daemonId, uuid: props.instanceId, target: currentPath, page: 0, page_size: 100, file_name: "" }
     });
     const items = res.value?.items || [];
 
