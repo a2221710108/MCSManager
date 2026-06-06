6849e9fe8259cb2b348ad98ca1b6d3b78a62eacd
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-06-06 16:34:26 +0800

    Update general_stop.ts

--- daemon/src/entity/commands/general/general_stop.ts
+++ daemon/src/entity/commands/general/general_stop.ts
@@ -1,6 +1,7 @@
 import { $t } from "../../../i18n";
 import Instance from "../../instance/instance";
 import InstanceCommand from "../base/command";
+import GeneralKillCommand from "./GeneralKillCommand";
 
 export default class GeneralStopCommand extends InstanceCommand {
   constructor() {
@@ -25,16 +26,19 @@ export default class GeneralStopCommand extends InstanceCommand {
 
     const cacheStartCount = instance.startCount;
 
-    // If the instance is still in the stopped state after 10 minutes, restore the state
+    // 10 分钟超时后，若仍未停止，则强制结束进程
     setTimeout(() => {
       if (
         instance.status() === Instance.STATUS_STOPPING &&
         instance.startCount === cacheStartCount
       ) {
-        instance.println("ERROR", $t("TXT_CODE_general_stop.stopErr"));
-        instance.status(Instance.STATUS_RUNNING);
+        instance.println("WARN", $t("TXT_CODE_general_stop.stopTimeout"));
+        const killCmd = new GeneralKillCommand();
+        killCmd.exec(instance).catch((err) => {
+          instance.println("ERROR", `强制关闭实例失败: ${err}`);
+        });
       }
-    }, 1000 * 60 * 10);
+    }, 1000 * 60 * 1);
 
     return instance;
   }
