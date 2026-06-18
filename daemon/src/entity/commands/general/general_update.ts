820dfa6d04d426db58e23806e585ed7465b10cde
a2221710108 <49866312+a2221710108@users.noreply.github.com>
2026-06-18 22:00:57 +0800

    Update general_update.ts

--- daemon/src/entity/commands/general/general_update.ts
+++ daemon/src/entity/commands/general/general_update.ts
@@ -1,73 +1,74 @@
-import { $t } from "../../../i18n";
-import { InstanceUpdateAction } from "../../../service/instance_update_action";
-import logger from "../../../service/log";
-import Instance from "../../instance/instance";
-import InstanceCommand from "../base/command";
+import { $t } from "../../../i18n";  
+import { InstanceUpdateAction } from "../../../service/instance_update_action";  
+import logger from "../../../service/log";  
+import Instance from "../../instance/instance";  
+import InstanceCommand from "../base/command";  
 
-export default class GeneralUpdateCommand extends InstanceCommand {
-  private updateTask?: InstanceUpdateAction;
+export default class GeneralUpdateCommand extends InstanceCommand {  
+  private updateTask?: InstanceUpdateAction;  
 
-  constructor() {
-    super("GeneralUpdateCommand");
-  }
+  constructor() {  
+    super("GeneralUpdateCommand");  
+  }  
 
-  private stopped(instance: Instance) {
-    instance.asynchronousTask = undefined;
-    instance.setLock(false);
-    instance.status(Instance.STATUS_STOP);
-  }
+  private stopped(instance: Instance) {  
+    instance.asynchronousTask = undefined;  
+    instance.setLock(false);  
+    instance.status(Instance.STATUS_STOP);  
+  }  
 
-  async exec(instance: Instance) {
-    if (instance.status() !== Instance.STATUS_STOP && instance.status() !== Instance.STATUS_BUSY)
-      return instance.failure(new Error($t("TXT_CODE_general_update.statusErr_notStop")));
-    if (instance.asynchronousTask)
-      return instance.failure(new Error($t("TXT_CODE_general_update.statusErr_otherProgress")));
+  async exec(instance: Instance) {  
+    if (instance.status() !== Instance.STATUS_STOP && instance.status() !== Instance.STATUS_BUSY)  
+      return instance.failure(new Error($t("TXT_CODE_general_update.statusErr_notStop")));  
+    
+    if (instance.asynchronousTask)  
+      return instance.failure(new Error($t("TXT_CODE_general_update.statusErr_otherProgress")));  
 
-    let shouldAutoStart = true;
+    let hasError = false; // 新增：用來追蹤更新過程是否發生錯誤
 
-    try {
-      instance.setLock(true);
-      instance.asynchronousTask = this;
-      instance.status(Instance.STATUS_BUSY);
+    try {  
+      instance.setLock(true);  
+      instance.asynchronousTask = this;  
+      instance.status(Instance.STATUS_BUSY);  
+      this.updateTask = new InstanceUpdateAction(instance);  
+      await this.updateTask?.start();  
+      await this.updateTask?.wait();  
+    } catch (err: any) {  
+      hasError = true; // 發生錯誤，標記為 true
+      instance.println(  
+        $t("TXT_CODE_general_update.update"),  
+        $t("TXT_CODE_general_update.error", { err: err })  
+      );  
+    } finally {  
+      // 1. 恢復實體狀態為停止
+      this.stopped(instance);  
 
-      this.updateTask = new InstanceUpdateAction(instance);
-      await this.updateTask?.start();
-      await this.updateTask?.wait();
-    } catch (err: any) {
-      shouldAutoStart = false;
-      instance.println(
-        $t("TXT_CODE_general_update.update"),
-        $t("TXT_CODE_general_update.error", { err: err })
-      );
-    } finally {
-      this.stopped(instance);
-
-      if (shouldAutoStart) {
-        // 延遲一小段時間確保資源完全釋放
-        setTimeout(() => {
-          instance.start().catch((startErr: any) => {
-            logger.error("Auto-start after update failed:", startErr);
-            instance.println("ERROR", $t("TXT_CODE_general_update.autoStartFailed", { err: startErr?.message || startErr }));
-          });
-        }, 1000);
+      // 2. 如果更新過程沒有發生錯誤 (且沒有被手動中斷)，則立即啟動伺服器
+      if (!hasError) {
+        try {
+          instance.println("INFO", $t("TXT_CODE_general_update.update") + " - Starting server..."); // 可選：印出開始啟動的提示
+          await instance.execPreset("start");
+        } catch (startErr: any) {
+          instance.failure(startErr);
+        }
       }
-    }
-  }
+    }  
+  }  
 
-  async stop(instance: Instance): Promise<void> {
-    instance.asynchronousTask = undefined;
-    logger.info(
-      $t("TXT_CODE_general_update.terminateUpdate", { instanceUuid: instance.instanceUuid })
-    );
-    instance.println(
-      $t("TXT_CODE_general_update.update"),
-      $t("TXT_CODE_general_update.terminateUpdate", { instanceUuid: instance.instanceUuid })
-    );
-    instance.println(
-      $t("TXT_CODE_general_update.update"),
-      $t("TXT_CODE_general_update.killProcess")
-    );
-    await this.updateTask?.stop();
-    this.updateTask = undefined;
-  }
+  async stop(instance: Instance): Promise<void> {  
+    instance.asynchronousTask = undefined;  
+    logger.info(  
+      $t("TXT_CODE_general_update.terminateUpdate", { instanceUuid: instance.instanceUuid })  
+    );  
+    instance.println(  
+      $t("TXT_CODE_general_update.update"),  
+      $t("TXT_CODE_general_update.terminateUpdate", { instanceUuid: instance.instanceUuid })  
+    );  
+    instance.println(  
+      $t("TXT_CODE_general_update.update"),  
+      $t("TXT_CODE_general_update.killProcess")  
+    );  
+    await this.updateTask?.stop();  
+    this.updateTask = undefined;  
+  }  
 }
